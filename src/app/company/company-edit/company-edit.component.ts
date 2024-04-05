f58a209aa1ebdc303a37769bb4725fc6b305c8eb
Matt Wicks <matt.wicks@gmail.com>
2024-04-05 15:10:45 +1100

    Add checkbox for adding phone number in company edit component

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -3,7 +3,9 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Company } from '../company';
 import { CompanyService } from '../company.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'fbc-company-edit',
   templateUrl: './company-edit.component.html',
@@ -28,14 +30,15 @@ export class CompanyEditComponent implements OnInit {
   companyForm = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     email: ['', [Validators.required, Validators.email]],
-    phone: [''],
+    checkPhone: [false],
+    phone: [{value: '', disabled: true}],
   });
 
   constructor(
     private fb: FormBuilder,
     private companyService: CompanyService,
     private router: Router,
-    private activateRoute: ActivatedRoute,
+    private activateRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
@@ -46,7 +49,34 @@ export class CompanyEditComponent implements OnInit {
       // TODO: handle loading scenario with a spinner or something else
       this.companyService
         .getCompany(this.companyId)
-        .subscribe(company => this.companyForm.patchValue(company));
+        .subscribe((company) => this.companyForm.patchValue(company));
+    }
+
+    this.setConditionalValidators();
+  }
+
+  private setConditionalValidators(): void {
+    const checkPhoneControl = this.companyForm.get('checkPhone');
+
+    if (checkPhoneControl) {
+      checkPhoneControl.valueChanges
+        .pipe(untilDestroyed(this))
+        .subscribe((checked) => {
+          const phoneControl = this.companyForm.get('phone');
+
+          if (!phoneControl) {
+            return;
+          }
+
+          if (checked) {
+            phoneControl.setValidators(Validators.required);
+            phoneControl.enable();
+          } else {
+            phoneControl.clearValidators();
+            phoneControl.disable();
+          }
+          phoneControl.updateValueAndValidity();
+        });
     }
   }
 
@@ -77,6 +107,5 @@ export class CompanyEditComponent implements OnInit {
     //     this.router.navigateByUrl('/company/list');
     //   });
     // }
-
   }
 }
