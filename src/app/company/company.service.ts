a3cd31bc79cb13d2e133ee9703ff081eada4f927
Matt Wicks <matt.wicks@gmail.com>
2024-04-05 11:54:02 +1100

    Added error handling in CompanyService

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Company } from './company';
 import { HttpClient } from '@angular/common/http';
-import { Observable, ObservedValueOf } from 'rxjs';
+import { Observable, ObservedValueOf, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +15,15 @@ export class CompanyService {
   ) { }
 
   getCompanies(): Observable<Company[]> {
-    return this.httpClient.get<Company[]>(`${this.API_BASE}/company`);
+    return this.httpClient.get<Company[]>(`${this.API_BASE}/company`).pipe(
+      catchError(this.errorHandler),
+    );
+  }
+
+  // 1. use http interceptor and move this logic to a global handler
+  // 2. return an empty observable - means its completed
+  private errorHandler(error: Error): Observable<Company[]> {
+    console.error('implement custom error handler here', error);
+    return new Observable<Company[]>();
   }
 }
