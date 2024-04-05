65f1677cd09472b9731e348a8ba3e352e41d9b33
Matt Wicks <matt.wicks@gmail.com>
2024-04-05 12:49:35 +1100

    Add routes for CompanyListComponent and CompanyEditComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'company/list' },
+  { path: 'company/list', component: CompanyListComponent },
+  { path: 'company/add', component: CompanyEditComponent },
+  { path: 'company/edit/:id', component: CompanyEditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/company/company-edit/company-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fbc-company-edit',
+  templateUrl: './company-edit.component.html',
+  styleUrl: './company-edit.component.scss'
+})
+export class CompanyEditComponent {
+
+}
